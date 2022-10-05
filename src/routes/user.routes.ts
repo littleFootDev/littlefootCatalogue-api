5f7010acb6f763d0fbe625e32af0b5583d40a3e8
littleFootDev <thelittlefootdev@gmail.com>
2022-10-05 07:51:06 +0200

    Adding CRUD for user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,18 @@
 import {Router} from 'express';
 
 import {signUp} from '../controllers/users/auth.controller';
+import{getAllUsers, getOneUser, updateUser, deleteUser} from '../controllers/users/user.controller';
 
 const userRouter = Router();
-//Auth Route
 
+//Auth Route
 userRouter.post('/signup', signUp);
+
+//User Route
+userRouter.get('/', getAllUsers);
+userRouter.get('/:id', getOneUser);
+userRouter.patch('/update/:id', updateUser);
+userRouter.delete('/delete/:id', deleteUser);
 
 
 
